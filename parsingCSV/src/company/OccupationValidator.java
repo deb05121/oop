62b3314cc713dfadbaab64f7cf33b7ad8ecf447f
deb05121 <129320702+deb05121@users.noreply.github.com>
2023-09-26 19:29:17 +0200

    Valid test is fixed.

--- parsingCSV/src/company/OccupationValidator.java
+++ parsingCSV/src/company/OccupationValidator.java
@@ -3,6 +3,6 @@ package company;
 public class OccupationValidator extends Validator<String> {
     @Override
     boolean isValid(String text) {
-        return text.matches("[a-z]+([ ][a-z]*)*");
+        return text.matches("[a-z]+([ ][a-z]+)*");
     }
 }
