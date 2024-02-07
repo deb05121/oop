43d2e5725b3cb90d8cc75a4f99c7e4ec4f3f48c3
deb05121 <129320702+deb05121@users.noreply.github.com>
2024-02-07 20:08:18 +0100

    I refactored test kode.

--- ClubMembersSortedCF/src/members/v1/ZipCodeValidatorTest.java
+++ ClubMembersSortedCF/src/members/v1/ZipCodeValidatorTest.java
@@ -3,16 +3,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class zipCodeValidatorTest {
+public class ZipCodeValidatorTest {
     @ParameterizedTest
-    @ValueSource(ints = {999, 0123, 12345,})
+    @ValueSource(ints = {999, 0123, 12345})
     void zipCodeValidatorTestA(int number){
         ZipCodeValidator zipCodeValidator = new ZipCodeValidator();
         Assertions.assertFalse(zipCodeValidator.isValid(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {9999, 1000, 1234,})
+    @ValueSource(ints = {9999, 1000, 1234})
     void zipCodeValidatorTestB(int number){
         ZipCodeValidator zipCodeValidator = new ZipCodeValidator();
         Assertions.assertTrue(zipCodeValidator.isValid(number));
