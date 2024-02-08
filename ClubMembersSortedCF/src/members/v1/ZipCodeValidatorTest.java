001848903c666775d22fe29af8c9570ccc8f3bea
deb05121 <129320702+deb05121@users.noreply.github.com>
2024-02-08 18:07:12 +0100

    Club members project is refactored.

--- ClubMembersSortedCF/src/members/v1/ZipCodeValidatorTest.java
+++ ClubMembersSortedCF/src/members/v1/ZipCodeValidatorTest.java
@@ -1,20 +1,22 @@
 package members.v1;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class ZipCodeValidatorTest {
+    ZipCodeValidator zipCodeValidator = new ZipCodeValidator();
+
     @ParameterizedTest
     @ValueSource(ints = {999, 0123, 12345})
     void zipCodeValidatorTestA(int number){
-        ZipCodeValidator zipCodeValidator = new ZipCodeValidator();
         Assertions.assertFalse(zipCodeValidator.isValid(number));
     }
 
     @ParameterizedTest
     @ValueSource(ints = {9999, 1000, 1234})
     void zipCodeValidatorTestB(int number){
-        ZipCodeValidator zipCodeValidator = new ZipCodeValidator();
         Assertions.assertTrue(zipCodeValidator.isValid(number));
     }
 
