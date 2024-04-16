86b0a7da580cd9007bfabea741e390438ced02e8
deb05121 <129320702+deb05121@users.noreply.github.com>
2024-04-16 09:45:44 +0200

    How many products query

--- webshopOOP/src/Main.java
+++ webshopOOP/src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import webshop.MenuAction;
+import webshop.query.HowManyProducts;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // database connection
+        Properties properties = new Properties();
+        properties.load(Main.class.getResourceAsStream("application.properties"));
+        String user = properties.getProperty("db_user");
+        String password = properties.getProperty("db_pass");
+        String url = properties.getProperty("db_url");
+
+        HowManyProducts howManyProducts = new HowManyProducts();
+
+        MenuAction[] menuActions = {
+
+        };
+        try (Connection connection
+                     = DriverManager.getConnection(url, user, password);
+             Menu menu = new Menu(connection, menuActions)) {
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- webshopOOP/src/webshop/query/HowManyProducts.java
+++ webshopOOP/src/webshop/query/HowManyProducts.java
@@ -3,8 +3,10 @@ package webshop.query;
 import webshop.MenuAction;
 
 public class HowManyProducts implements MenuAction {
+    private static final String SELECT_TYPES_OF_PRODUCTS_WITHOUT_0_QUANTITY = "SELECT COUNT(item_name) FROM webshop.shop_item WHERE quantity <> 0";
     @Override
     public void close() throws Exception {
 
     }
+
 }
